71773de8c4deab65314e81a595f8fc66132c1208
Patrick Kennedy <pakattack161@gmail.com>
2024-07-08 11:19:44 -0700

    add OhMyPosh to the dependencies

--- Profile.ps1
+++ Profile.ps1
@@ -9,7 +9,8 @@ $DependencyIDs = @(
   "Docker.DockerDesktop",
   "voidtools.Everything",
   "Microsoft.PowerToys",
-  "FastStone.Capture"
+  "FastStone.Capture",
+  "JanDeDobbeleer.OhMyPosh"
 )
 
 $DesktopDependencies = @(
